4efccd69a861f40ea682329fec4ee1edc292f356
Jonas Myrenås <jonas.myrenas@mindler.se>
2023-01-14 13:02:10 +0100

    better migration

--- src/migrations/migrate.ts
+++ src/migrations/migrate.ts
@@ -1,6 +1,5 @@
 import { addDays, startOfDay } from "date-fns";
 import { GAME } from "../config/game";
-import { useBirdStreakStore } from "../hooks/useBirdStreakStore";
 import { BirdStreakStore } from "../types";
 
 export const migrate = (rehydratedGame: any): BirdStreakStore => {
@@ -10,19 +9,29 @@ export const migrate = (rehydratedGame: any): BirdStreakStore => {
 
   // Migrate bugfix for periodDate
   if (major === 0 && minor <= 3 && patch < 1) {
-    const lastItem = rehydratedGame.list[rehydratedGame.list.length - 1];
-    const newGame = {
-      ...rehydratedGame,
-      // update periodStart
-      periodStart: startOfDay(addDays(lastItem.periodEnd, 1)),
+    console.log("migrate to 0.3.1");
 
-      // remove legacy nextPeriodStarts
+    interface OldBirdStreakStore030 extends BirdStreakStore {
+      nextPeriodStarts: any;
+    }
+
+    const lastItem = rehydratedGame.list[rehydratedGame.list.length - 1];
+
+    const newGame: OldBirdStreakStore030 = {
+      ...rehydratedGame,
     };
+    // update periodStart
+    newGame.periodStart = startOfDay(addDays(lastItem.periodEnd, 1));
+
+    // remove legacy nextPeriodStarts
     delete newGame.nextPeriodStarts;
-    localStorage.setItem(
-      GAME.persistKey,
-      JSON.stringify(useBirdStreakStore.getState())
-    );
+
+    // Update appVersion
+    newGame.appVersion = `${process.env.REACT_APP_VERSION}`;
+
+    // persist new data
+    localStorage.setItem(GAME.persistKey, JSON.stringify(newGame));
+
     return newGame as BirdStreakStore;
   }
 
