5600858b7274928d330caee6bee5af5a9b8cd622
Jonas Myrenås <jonas.myrenas@mindler.se>
2023-01-14 12:42:05 +0100

    cleanup

--- src/migrations/migrate.ts
+++ src/migrations/migrate.ts
@@ -8,11 +8,8 @@ export const migrate = (rehydratedGame: any): BirdStreakStore => {
     .split(".")
     .map((v: string) => parseInt(v, 10));
 
-  console.log("!!!", major, minor, patch);
-
   // Migrate bugfix for periodDate
   if (major === 0 && minor <= 3 && patch < 1) {
-    console.log(111);
     const lastItem = rehydratedGame.list[rehydratedGame.list.length - 1];
     const newGame = {
       ...rehydratedGame,
@@ -22,12 +19,10 @@ export const migrate = (rehydratedGame: any): BirdStreakStore => {
       // remove legacy nextPeriodStarts
     };
     delete newGame.nextPeriodStarts;
-    console.log(222);
     localStorage.setItem(
       GAME.persistKey,
       JSON.stringify(useBirdStreakStore.getState())
     );
-    console.log(333);
     return newGame as BirdStreakStore;
   }
 
