71826371f5275f160ca454f589957aa1ff843648
Jonas Myrenås <jonas.myrenas@mindler.se>
2023-01-14 22:22:08 +0100

    different period lengths

--- src/config/game.ts
+++ src/config/game.ts
@@ -2,6 +2,5 @@ export const GAME = {
   persistKey: "game",
   legacyGamesKey: "legacyGames",
   streakSpanMillis: 2 * 24 * 60 * 60 * 1000,
-  // streakSpanMillis: 5 * 1000,
   checkInterval: 1000,
 };
